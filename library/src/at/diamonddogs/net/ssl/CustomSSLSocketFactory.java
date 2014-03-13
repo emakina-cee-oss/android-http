1fc113389a62ea0f6c75befd709b05d995c497b0
D.Hoeggerl <hoeggerl.daniel@gmail.com>
2014-03-13 16:41:47 +0100

    added licence header and better error handling @CustomSSLSocketFactory

--- library/src/at/diamonddogs/net/ssl/CustomSSLSocketFactory.java
+++ library/src/at/diamonddogs/net/ssl/CustomSSLSocketFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2012 the diamond:dogs|group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package at.diamonddogs.net.ssl;
 
 import java.io.IOException;
@@ -51,8 +66,7 @@ public class CustomSSLSocketFactory implements SocketFactory, LayeredSocketFacto
 	 */
 	@Override
 	public Socket connectSocket(Socket sock, String host, int port, InetAddress localAddress, int localPort, HttpParams params)
-
-	throws IOException, UnknownHostException, ConnectTimeoutException {
+			throws IOException, UnknownHostException, ConnectTimeoutException {
 		int connTimeout = HttpConnectionParams.getConnectionTimeout(params);
 		int soTimeout = HttpConnectionParams.getSoTimeout(params);
 		InetSocketAddress remoteAddress = new InetSocketAddress(host, port);
@@ -77,7 +91,15 @@ public class CustomSSLSocketFactory implements SocketFactory, LayeredSocketFacto
 	 */
 	@Override
 	public Socket createSocket() throws IOException {
-		return sslcontext.getSocketFactory().createSocket();
+		if (sslcontext != null) {
+			return sslcontext.getSocketFactory().createSocket();
+		} else {
+			try {
+				return SSLContext.getDefault().getSocketFactory().createSocket();
+			} catch (Exception e) {
+				return null;
+			}
+		}
 	}
 
 	/**
@@ -94,7 +116,15 @@ public class CustomSSLSocketFactory implements SocketFactory, LayeredSocketFacto
 	 */
 	@Override
 	public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
-		return sslcontext.getSocketFactory().createSocket(socket, host, port, autoClose);
+		if (sslcontext != null) {
+			return sslcontext.getSocketFactory().createSocket(socket, host, port, autoClose);
+		} else {
+			try {
+				return SSLContext.getDefault().getSocketFactory().createSocket(socket, host, port, autoClose);
+			} catch (Exception e) {
+				return null;
+			}
+		}
 	}
 
 	// -------------------------------------------------------------------
